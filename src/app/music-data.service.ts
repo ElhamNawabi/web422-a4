64a7ede06482c99c51fca21ffa13abdc106c5ae1
ElhamNawabi <nawabi.elham@gmail.com>
2022-04-21 20:51:52 -0400

    commit

--- src/app/music-data.service.ts
+++ src/app/music-data.service.ts
@@ -17,13 +17,13 @@ export class MusicDataService {
   getNewReleases(): Observable<SpotifyApi.ListOfNewReleasesResponse> {
       return this.spotifyToken.getBearerToken().pipe(mergeMap(token=>{
         return this.http.get<SpotifyApi.ListOfNewReleasesResponse>("https://api.spotify.com/v1/browse/new-releases", { headers: { "Authorization": `Bearer ${token}` } });
-      }));
+      }))
   }
 
   getArtistsById(id: string): Observable<SpotifyApi.ArtistsAlbumsResponse> {
     return this.spotifyToken.getBearerToken().pipe(mergeMap(token=>{
       return this.http.get<SpotifyApi.ArtistsAlbumsResponse>(`https://api.spotify.com/v1/artists/${id}`, { headers: { "Authorization": `Bearer ${token}` } });
-    }));
+    }))
   }
 
   getAlbumsByArtistId(id: any): Observable<SpotifyApi.ArtistsAlbumsResponse> {
@@ -32,10 +32,10 @@ export class MusicDataService {
         return this.http.get<SpotifyApi.ArtistsAlbumsResponse>(
           `https://api.spotify.com/v1/artists/${id}/albums?include_groups=album,single&limit=50`,
           { headers: { "Authorization": `Bearer ${token}` } }
-        );
+        )
       })
-    );
-  };
+    )
+  }
 
   getAlbumById(id: any): Observable<SpotifyApi.SingleAlbumResponse> {
     return this.spotifyToken.getBearerToken().pipe(
@@ -43,21 +43,21 @@ export class MusicDataService {
         return this.http.get<SpotifyApi.SingleAlbumResponse>(
           `https://api.spotify.com/v1/albums/${id}`,
           { headers: { "Authorization": `Bearer ${token}` } }
-        );
+        )
       })
-    );
-  };
+    )
+  }
 
   searchArtists(searchString: string): Observable<SpotifyApi.ArtistSearchResponse> {
     return this.spotifyToken.getBearerToken().pipe(
       mergeMap((token) => {
         return this.http.get<SpotifyApi.ArtistSearchResponse>(
-          `https://api.spotify.com/v1/albums/q=${searchString}&type=artist&limit=50`,
+          `https://api.spotify.com/v1/search?q=${searchString}&type=artist&limit=50`,
           { headers: { "Authorization": `Bearer ${token}` } }
-        );
+        )
       })
-    );
-  };
+    )
+  }
 
   addToFavourites(id:string): Observable<[String]> {
    
@@ -69,13 +69,13 @@ export class MusicDataService {
       if (favouritesArray.length> 0) {
         return this.spotifyToken.getBearerToken().pipe(mergeMap((token)=> {
           return this.http.get<any>(`https://api.spotify.com/v1/tracks?ids=${favouritesArray.join()}`, { headers: { Authorization: `Bearer ${token}`} });
-      }));
+      }))
     } else {
         return new Observable((o)=> { o.next({tracks: []});
-      });
+      })
     }
     })
-    );
+    )
   }
 
   getFavourites(): Observable<any> {
@@ -85,11 +85,13 @@ export class MusicDataService {
         return this.spotifyToken.getBearerToken().pipe(mergeMap((token)=> {
         return this.http.get<any>(`https://api.spotify.com/v1/tracks?ids=${favouritesArray.join()}`, { headers: { Authorization: `Bearer ${token}`} } );
       })
-      );
-      } else {
+      )
+      } 
+      else 
+      {
         return new Observable((o)=> { o.next({tracks: []}); });
       }
-    }));
+    }))
   }
 
 }
